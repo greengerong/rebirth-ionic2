58d088d6e466f6b666eb1a48b518db67c09a289e
polang <grzx2210@163.com>
2016-11-27 22:34:44 +0800

    [feature] show loading for refresh

--- src/components/article-list/article-list.component.ts
+++ src/components/article-list/article-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy, Output } from '@angular/core';
 import { ArticleService, SearchResult, Article } from '../../core';
 import { environment } from '../../environments/environment';
 import { EventEmitter } from '@angular/common/src/facade/async';
-import { ToastController } from 'ionic-angular';
+import { ToastController, LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'article-list',
@@ -16,7 +16,8 @@ export class ArticleListComponent implements OnInit,OnDestroy {
   @Output()
   articleClicked = new EventEmitter();
 
-  constructor(private articleService: ArticleService, private toastCtrl: ToastController) {
+  constructor(private articleService: ArticleService, private toastCtrl: ToastController,
+              private loadingCtrl: LoadingController) {
 
   }
 
@@ -40,38 +41,49 @@ export class ArticleListComponent implements OnInit,OnDestroy {
 
   refresh(refresher) {
     this.pageIndex = 1;
+    let loading = this.loadingCtrl.create({
+      content: '数据刷新中...'
+    });
+    loading.present();
     (<any>this.articleService.getAllArticles).cacheEvict();
     this.articleService.getArticles(this.pageIndex, environment.article.pageSize)
       .subscribe(result => {
-        this.article = result;
-        refresher.complete();
-        this.toastCtrl.create({
-          message: '数据刷新成功',
-          duration: 1000
-        }).present();
-      }, (e) => console.log(e, 'ArticleListComponent error'));
+          this.article = result;
+          this.toastCtrl.create({
+            message: '数据刷新成功',
+            duration: 1000
+          }).present();
+
+        },
+        (e) => console.log(e, 'ArticleListComponent error'),
+        () => {
+          refresher.complete();
+          loading.dismiss();
+        });
   }
 
   pageChange(pageIndex, done?: () => void) {
     this.articleService.getArticles(pageIndex, environment.article.pageSize)
       .subscribe(result => {
-        if (!this.article) {
-          this.article = result;
-          return;
-        }
+          if (!this.article) {
+            this.article = result;
+            return;
+          }
 
-        this.article.pageIndex = result.pageIndex;
-        this.article.total = result.total;
-        this.article.result.push(...result.result);
+          this.article.pageIndex = result.pageIndex;
+          this.article.total = result.total;
+          this.article.result.push(...result.result);
 
-        setTimeout(() => {
-          done && done();
           this.toastCtrl.create({
             message: `第${pageIndex}页数据加载完成`,
             duration: 1000
           }).present();
-        }, 500);
-      }, (e) => console.log(e, 'ArticleListComponent error'));
+
+        },
+        (e) => console.log(e, 'ArticleListComponent error'),
+        ()=> {
+          done && done();
+        });
   }
 
 }
